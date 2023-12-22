947ed9b08edb4468a99ddb12a3b54f41c99462fb
LunarLife <ivan.andreychenko1502@gmail.com>
2023-12-22 19:01:29 +0100

    Inventory interact update.

--- Api/Components/Inventories/IFunctionalInventory.cs
+++ Api/Components/Inventories/IFunctionalInventory.cs
@@ -5,11 +5,21 @@ namespace SkyBots.Api.Components.Inventories;
 
 public interface IFunctionalInventory
 {
-    public IInventoryMoveSlotTask Move(Bot bot, InventoryTarget target, int source, int destination, int count);
+    public IInventoryMoveSlotTask Move(Bot bot, int source, int destination, int count);
+    public IInventoryMoveSlotTask Take(Bot bot, int source, int destination, int count);
+    public IInventoryMoveSlotTask Put(Bot bot, int source, int destination, int count);
 }
 
 public interface IFunctionalInventory<in T> : IFunctionalInventory where T : Enum
 {
-    public IInventoryMoveSlotTask Move(Bot bot, InventoryTarget target, int source, T destination, int count) =>
-        Move(bot, target, source, Convert.ToInt32(destination), count); 
+    public IInventoryMoveSlotTask Move(Bot bot, int source, T destination, int count) =>
+        Move(bot, source, Convert.ToInt32(destination), count);
+
+    public IInventoryMoveSlotTask Take(Bot bot, int source, T destination, int count) =>
+        Take(bot, source, Convert.ToInt32(destination), count);
+
+    public IInventoryMoveSlotTask Put(Bot bot, int source, T destination, int count) =>
+        Put(bot, source, Convert.ToInt32(destination), count);
+
+
 }
